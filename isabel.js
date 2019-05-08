c081ef673b8ca9ca74718e34b1bf0fdef82bd292
Mark Chang <chang.m.mark@gmail.com>
2019-05-08 19:43:30 +0000

    Automatically backed up by Learn

---
+++ isabel.js
@@ -0,0 +1,13 @@
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1) // "Hello, 1!"
+ 
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`)
+}
